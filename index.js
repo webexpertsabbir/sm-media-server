add7a2d93c319f9609ac84f2583f93486326cd78
Sabbir Hossen <webexpertsabbir@gmail.com>
2022-12-28 00:06:07 +0600

    data post user post

--- index.js
+++ index.js
@@ -17,14 +17,34 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sabbir.0dgpj5g.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+    try{
+        const usersPostsCollection = client.db('smMedia').collection('userPosts');
+
+        app.get('/posts', async (req, res) => {
+            const query = {};
+            const posts = await usersPostsCollection.find(query).toArray();
+            res.send(posts);
+        });
+        
+
+        app.post('/posts', async (req, res) => {
+            const post = req.body;
+            console.log(post);
+            const result = await usersPostsCollection.insertOne(post);
+            res.send(result);
+        });
+        
+
+    }
+
+    finally{
+
+    }
+}
+run().catch(console.log);
 
 app.get('/', async (req, res) => {
     res.send('sm-media server running');
